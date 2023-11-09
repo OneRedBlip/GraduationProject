1d9be40a181730ed9f3c6fa51a129dc579667768
AbdulAlkhalaf <150266442+AbdulAlkhalaf@users.noreply.github.com>
2023-11-09 21:24:50 +0300

    code of our project T2T
    1-first page (add pic)
    2- login and regestiration page
    3-home page consist of requast page (7arage)
    4-rewarde page

--- trash2treasure/lib/main.dart
+++ trash2treasure/lib/main.dart
@@ -1,117 +1,303 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
 
 void main() {
-  runApp(CoolDemoApp());
+  runApp(TrashToTreasureApp());
 }
 
-class CoolDemoApp extends StatelessWidget {
-  const CoolDemoApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: LoginPage());
-  }
-}
-
-class FancyHomeScreen extends StatefulWidget {
-  const FancyHomeScreen({super.key});
-
-  @override
-  State<FancyHomeScreen> createState() => _FancyHomeScreenState();
-}
-
-class _FancyHomeScreenState extends State<FancyHomeScreen> {
-  int count = 0;
+class TrashToTreasureApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("this is the title"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text("First item!"),
-              Icon(Icons.add),
-              Text("$count"),
-              const Text("Last item!"),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-          setState(() {
-                      count++;
-                    });
-          },
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
+      title: 'Trash to Treasure',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignupPage(),
+        '/requests': (context) => RequestsPage(),
+        '/profile': (context) => UserProfilePage(),
+        '/recycle-info': (context) => RecycleInfoPage(),
+        '/rewards': (context) => RewardsPage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trash to Treasure'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/image/unnamed.png'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text('Continue'),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.api),
-              label: "diamond",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.attractions),
-              label: "attractions",
-            )
           ],
-          onTap: (value) {
-            switch (value) {
-              case 1:
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => DiamondPage(),
-                  ),
-                );
-                break;
-              case 2:
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (_) => LoginPage()));
-              default:
-            }
-          },
-        ),
-        drawer: const Drawer(
-          child: Text(
-            "Hello!",
-            textScaleFactor: 2.5,
-          ),
         ),
       ),
     );
   }
 }
-
-void clicktyclick() {
-  print("This is the function!");
-}
-
-class DiamondPage extends StatelessWidget {
-  const DiamondPage({super.key});
-
+class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: (const Text("This is the diamond page.")),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Repeat Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Sign up logic
+              },
+              child: Text('Continue'),
+            ),
+          ],
         ),
-        body: Center(
-            child: IconButton.filled(
-                iconSize: 50,
-                onPressed: () => {Navigator.pop(context)},
-                icon: const Icon(Icons.arrow_back))),
       ),
     );
   }
 }
+
+class RequestsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Requests'),
+      ),
+      body: Center(
+        child: Text('Requests Page'),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            TextButton(
+              onPressed: () {
+                // Forgot password logic
+              },
+              child: Text('Forgot my password'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Login logic
+              },
+              child: Text('Enter'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/signup');
+              },
+              child: Text('Sign up'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Help and support logic
+              },
+              child: Text('Help and Support'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class UserProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              // Add logic to display user's profile photo
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Logic to change user's photo
+              },
+              child: Text('Change Photo'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Logic to change username
+              },
+              child: Text('Change Username'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Logic to change phone number
+              },
+              child: Text('Change Phone Number'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              icon: Icon(Icons.home),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/profile');
+              },
+              icon: Icon(Icons.person),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/recycle-info');
+              },
+              icon: Icon(Icons.info),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RecycleInfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recycle Information'),
+      ),
+      body: Center(
+        child: Text('Display recycling information here'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              icon: Icon(Icons.home),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/profile');
+              },
+              icon: Icon(Icons.person),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/recycle-info');
+              },
+              icon: Icon(Icons.info),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RewardsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rewards'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Points: 1500'),
+            // Add rewards and incentives UI
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.arrow_back),
+      ),
+    );
+  }
+}
+
