6b85e11fe05e902eb1ba3df39cb72af4619cc73c
AbdulAlkhalaf <150266442+AbdulAlkhalaf@users.noreply.github.com>
2023-11-18 14:28:32 +0300

    abdul changes
    Here I created the main page , which is include the requests also includes the navigation bar in the bottom that consist of information guidelines rewards profile doing all that kind of stuff and I was trying to link it with the login page I did some changes there in the Login page

--- trash2treasure/lib/login_page.dart
+++ trash2treasure/lib/login_page.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,21 +17,21 @@ class LoginPage extends StatelessWidget {
       body: Column(
         children: [
           TextField(
-            controller:emailController ,
+            controller: emailController,
 
             decoration: InputDecoration(
               labelText: 'Username',
             ),
           ),
           TextField(
-            controller: passwordController ,
+            controller: passwordController,
             decoration: InputDecoration(
               labelText: 'Password',
             ),
           ),
           ElevatedButton(
             onPressed: () {
-             log(emailController.text);
+              log(emailController.text);
             },
             child: Text('Login'),
           ),
@@ -46,8 +47,43 @@ class LoginPage extends StatelessWidget {
             },
             child: Text('Help/Support'),
           ),
+          ElevatedButton(
+            onPressed: () {
+              // Check login credentials here
+              String username = emailController.text;
+              String password = passwordController.text;
+
+              if (username == 'your_username' && password == 'your_password') {
+                // Navigate to MainPage if login is successful
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainPage()),
+                );
+              } else {
+                // Handle invalid login credentials
+                showDialog(
+                  context: context,
+                  builder: (context) =>
+                      AlertDialog(
+                        title: Text('Invalid Login'),
+                        content: Text('Please enter valid credentials.'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      ),
+                );
+              }
+            },
+            child: Text('Login'),
+          ),
         ],
       ),
     );
   }
 }
+
---
+++ trash2treasure/lib/main page.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TrashToTreasureApp());
+}
+
+class TrashToTreasureApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trash to Treasure',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(),
+      routes: {
+        '/info': (context) => InfoPage(),
+        '/reward': (context) => RewardPage(),
+        '/profile': (context) => ProfilePage(),
+        '/guidelines': (context) => GuidelinesPage(),
+      },
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trash to Treasure'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          RequestCard(
+            title: 'Request 1',
+            description: 'Description of Request 1',
+            onPressed: () {
+              Navigator.pushNamed(context, '/request1');
+            },
+          ),
+          RequestCard(
+            title: 'Request 2',
+            description: 'Description of Request 2',
+            onPressed: () {
+              Navigator.pushNamed(context, '/request2');
+            },
+          ),
+          RequestCard(
+            title: 'Request 3',
+            description: 'Description of Request 3',
+            onPressed: () {
+              Navigator.pushNamed(context, '/request3');
+            },
+          ),
+          RequestCard(
+            title: 'Request 4',
+            description: 'Description of Request 4',
+            onPressed: () {
+              Navigator.pushNamed(context, '/request4');
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'Info',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Reward',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Guidelines',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+        ],
+        currentIndex: 4, // Set the initial selected index (Home)
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.pushNamed(context, '/info');
+              break;
+            case 1:
+              Navigator.pushNamed(context, '/reward');
+              break;
+            case 2:
+              Navigator.pushNamed(context, '/profile');
+              break;
+            case 3:
+              Navigator.pushNamed(context, '/guidelines');
+              break;
+            case 4:
+            // Do nothing as we're already on the home page
+              break;
+          }
+        },
+      ),
+    );
+  }
+}
+
+class RequestCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final VoidCallback onPressed;
+
+  RequestCard({
+    required this.title,
+    required this.description,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(description),
+        onTap: onPressed,
+      ),
+    );
+  }
+}
+
+class InfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Info'),
+      ),
+      body: Center(
+        child: Text('Information page content'),
+      ),
+    );
+  }
+}
+
+class RewardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reward'),
+      ),
+      body: Center(
+        child: Text('Rewards page content'),
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Text('Profile page content'),
+      ),
+    );
+  }
+}
+
+class GuidelinesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Guidelines'),
+      ),
+      body: Center(
+        child: Text('Guidelines page content'),
+      ),
+    );
+  }
+}
