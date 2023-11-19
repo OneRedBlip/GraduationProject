9f9727ce6964d6098405ad9ad461fdfff17192c3
OneRedBlip <149704244+OneRedBlip@users.noreply.github.com>
2023-11-19 03:13:59 +0300

    server: implement login.

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, session
 import database
 
 app = Flask(__name__)
@@ -24,6 +24,22 @@ def signup():
         data = request.get_json()
         db.insert("users", data)
         return ("User created successfully", 201)
+    except Exception as e:
+        print("Exception: ", e)
+        # TODO Return a better status code
+        return ("An error has occured", 500)
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    try:
+        data = request.get_json()
+        userInDB = db.getUserPass(data["user_name"])
+        if userInDB is not None and userInDB[1] == data["password"]:
+            session["user_id"] = userInDB[0]
+            return db.getUserInfo(userInDB[0])
+        else:
+            return ("Invalid credentials", 401)
     except Exception as e:
         print("Exception: ", e)
         # TODO Return a better status code
