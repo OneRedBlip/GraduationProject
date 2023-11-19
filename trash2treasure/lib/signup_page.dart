5c5e09978c253b4bc2670a1818b9e0202d92422f
OneRedBlip <149704244+OneRedBlip@users.noreply.github.com>
2023-11-19 20:27:50 +0300

    implement postSignup function

--- trash2treasure/lib/signup_page.dart
+++ trash2treasure/lib/signup_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SignupPage extends StatelessWidget {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController  = TextEditingController();
-  final TextEditingController passwordRepeatController  = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController passwordRepeatController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +29,7 @@ class SignupPage extends StatelessWidget {
             ),
           ),
           TextField(
-          controller: passwordController,
+            controller: passwordController,
             decoration: InputDecoration(
               labelText: 'Password',
             ),
@@ -49,3 +52,25 @@ class SignupPage extends StatelessWidget {
   }
 }
 
+Future<http.Response> postSignup(String userName, String passWord, string ) async {
+  Map<String, dynamic> requestBody = {
+    'user_name': userName,
+    'password': passWord
+  };
+
+  try {
+    final response = await http.post(
+      Uri(host: "127.1.1.1", port: 8000, scheme: "http", path: "/signup"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(requestBody),
+    );
+
+    return response;
+  } catch (error) {
+    //TODO Return something better
+    return http.Response(
+        'An error occured while attempting to connect to server', 500);
+  }
+}
