69e37a506a80f046f846ca9afd234fa81be23e6c
OneRedBlip <149704244+OneRedBlip@users.noreply.github.com>
2023-11-19 16:16:42 +0300

    import http in login_page

--- trash2treasure/lib/login_page.dart
+++ trash2treasure/lib/login_page.dart
@@ -3,6 +3,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'rquests_page.dart';
 class LoginPage extends StatefulWidget {
