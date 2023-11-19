a2247415667c9ffbc4e71e01eec81db98bb1babb
AbdulAlkhalaf <150266442+AbdulAlkhalaf@users.noreply.github.com>
2023-11-19 23:46:06 +0300

    2023/11/19
    working on the requests_page.dart i had added the nev bar and some of the code had some problem only the profile i can open it you can find the code then copy it and past it in the requests page like i did with the reward but it does not work

--- trash2treasure/lib/rquests_page.dart
+++ trash2treasure/lib/rquests_page.dart
@@ -104,6 +104,7 @@ class RequestsPage extends StatelessWidget {
             label: 'Home',
           ),
         ],
+        type: BottomNavigationBarType.fixed,
         currentIndex: 4, // Set the initial selected index (Home)
         onTap: (index) {
           switch (index) {
@@ -171,10 +172,35 @@ class RewardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Reward'),
+        title: Text('Rewards'),
       ),
       body: Center(
-        child: Text('Rewards page content'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Congratulations!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'You have earned 100 points!',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Redeem rewards logic goes here
+              },
+              child: Text('Redeem Now'),
+            ),
+          ],
+        ),
       ),
     );
   }
