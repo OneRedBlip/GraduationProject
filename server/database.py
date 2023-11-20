06b93c0e33338ce24cd34334330666110cf8791d
OneRedBlip <149704244+OneRedBlip@users.noreply.github.com>
2023-11-20 17:51:07 +0300

    Server: fix type hints

--- server/database.py
+++ server/database.py
@@ -117,27 +117,27 @@ class Database:
             "SELECT  center_name, material_type, location, hours, additional_info FROM centers WHERE center_id = ?", (center_id,)).fetchone()
         return res
 
-    def getCentersInLocation(self, location: str, ) -> tuple[int, str, str, str, dict, str]:  # Return types incorrect 🤷.
+    def getCentersInLocation(self, location: str, ) -> tuple[tuple, ...]:
         res = self.cur.execute(
             "SELECT center_id, center_name, material_type, location, hours, additional_info FROM centers WHERE location = ?", (location,)).fetchall()
         # using fetchall is kinda wrong. but should be fine for this prototype.
         return res
 
-    def getPostsInLocation(self, location: str, ) -> tuple[int, int, int, str, str, str, str]:  # Return types incorrect 🤷.
+    def getPostsInLocation(self, location: str, ) -> tuple[tuple, ...]:
         res = self.cur.execute(
             "SELECT post_id, user_id, post_date, material_type, location, additional_info, status FROM posts WHERE location = ?", (location,)).fetchall()
         # using fetchall is kinda wrong. but should be fine for this prototype.
         return res
 
-    def getPostsByMaterial(self, material: str, ) -> tuple[int, int, int, str, str, str, str]:  # Return types incorrect 🤷.
+    def getPostsByMaterial(self, material: str, ) -> tuple[tuple, ...]:
         res = self.cur.execute(
             "SELECT post_id, user_id, post_date, material_type, location, additional_info, status FROM posts WHERE material = ?", (material,)).fetchall()
         # using fetchall is kinda wrong. but should be fine for this prototype.
         return res
 
-    def getPostsByMaterialAndLocation(self, material: str, location: str) -> tuple[int, int, int, str, str, str, str]:  # Return types incorrect 🤷.
+    def getPostsByMaterialAndLocation(self, material: str, location: str) -> tuple[tuple, ...]:
         res = self.cur.execute(
-            "SELECT post_id, user_id, post_date, material_type, location, additional_info, status FROM posts WHERE material = ? AND location = ?", (material,location)).fetchall()
+            "SELECT post_id, user_id, post_date, material_type, location, additional_info, status FROM posts WHERE material = ? AND location = ?", (material, location)).fetchall()
         # using fetchall is kinda wrong. but should be fine for this prototype.
         return res
 
