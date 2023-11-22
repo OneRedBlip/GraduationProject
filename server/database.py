6f0b189da00b2875f8f396b8bdb6bbe0ab61b72a
OneRedBlip <149704244+OneRedBlip@users.noreply.github.com>
2023-11-23 02:34:12 +0300

    Server: fix and format insert function

--- server/database.py
+++ server/database.py
@@ -82,17 +82,23 @@ class Database:
     def insert(self, table_name: str, data: dict) -> None:
         match table_name:
             case "users":
-                query = "INSERT INTO users ('user_name', email, phone_num, password, user_location) VALUES(:user_name, :email, :phone_num, :password, :user_location)"
+                query = '''INSERT INTO users ('user_name', email, phone_num, password, user_location)
+                           VALUES(:user_name, :email, :phone_num, :password, :user_location)'''
             case "rewards":
-                query = "INSERT INTO rewards VALUES(:reward_id, :reward_name, points_required, :reward_desc)"
+                query = '''INSERT INTO rewards (reward_name, points_required, reward_desc)
+                           VALUES(:reward_name, :points_required, :reward_desc)'''
             case "posts":
-                query = "INSERT INTO posts VALUES(:post_id, :user_id, :post_date, :material_type, :location, :additional_info, :status)"
+                query = '''INSERT INTO posts (user_id, post_date, material_type, location, additional_info, status)
+                           VALUES(:user_id, :post_date, :material_type, :location, :additional_info, :status)'''
             case "centers":
-                query = "INSERT INTO centers VALUES(:center_id, :center_name, :material_type, :location, :hours, :additional_info)"
+                query = '''INSERT INTO centers (center_name, material_type, location, hours, additional_info)
+                           VALUES(:center_name, :material_type, :location, :hours, :additional_info)'''
             case "rewards_users":
-                query = "INSERT INTO rewards_users VALUES(:reward_history_id, :user_id, :reward_id, :redeem_date, :redeemed_amount)"
+                query = '''INSERT INTO rewards_users (user_id, reward_id, redeem_date, redeemed_amount)
+                           VALUES(:user_id, :reward_id, :redeem_date, :redeemed_amount)'''
             case "users_centers":
-                query = "INSERT INTO users_center VALUES(:center_history_id :user_id, :center_id, :gain_date, :gained_amount)"
+                query = '''INSERT INTO users_center (user_id, center_id, gain_date, gained_amount)
+                           VALUES(:user_id, :center_id, :gain_date, :gained_amount)'''
             case _:
                 raise ValueError
                 # TODO throw a better exception
