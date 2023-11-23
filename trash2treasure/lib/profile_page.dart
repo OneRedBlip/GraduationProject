99455d232be16fde28238d1d31e759f77ee07dac
AbdulAlkhalaf <150266442+AbdulAlkhalaf@users.noreply.github.com>
2023-11-23 15:02:03 +0300

    moving the info inside the profile page

--- trash2treasure/lib/profile_page.dart
+++ trash2treasure/lib/profile_page.dart
@@ -1,5 +1,115 @@
 import 'package:flutter/material.dart';
 
+class InfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Info'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'The Green Saudi Vision 2030 is a plan by Saudi Arabia to achieve sustainable development. Recycling is a key component of this vision and is important for several reasons:',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Conservation of resources: Recycling helps preserve natural resources like forests and minerals by reducing the need for raw material extraction.',
+              style: TextStyle(fontSize: 14),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Energy savings: Recycling requires less energy compared to manufacturing from scratch, leading to reduced greenhouse gas emissions.',
+              style: TextStyle(fontSize: 14),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Waste reduction: Recycling diverts waste from landfills, extending the lifespan of materials and reducing pollution.',
+              style: TextStyle(fontSize: 14),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Environmental protection: Proper waste disposal through recycling minimizes soil and water contamination and reduces harm to wildlife.',
+              style: TextStyle(fontSize: 14),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Economic benefits: Recycling creates jobs, stimulates economic growth, and promotes a circular economy.',
+              style: TextStyle(fontSize: 14),
+            ),
+            SizedBox(height: 16),
+            Container(
+              alignment: Alignment.bottomRight,
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Contact Us',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(
+                            Icons.email,
+                            color: Colors.black,
+                          ),
+                          SizedBox(width: 8),
+                          Text('trash2treasure@gmail.com'),
+                        ],
+                      ),
+                      SizedBox(height: 4),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(
+                            Icons.photo,
+                            color: Colors.black,
+                          ),
+                          SizedBox(width: 8),
+                          Text('@trash2trasure'),
+                        ],
+                      ),
+                      SizedBox(height: 4),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(
+                            Icons.language,
+                            color: Colors.black,
+                          ),
+                          SizedBox(width: 8),
+                          Text('www.trash2treasure.com'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,26 +138,48 @@ class ProfilePage extends StatelessWidget {
                 border: Border.all(color: Colors.grey),
                 borderRadius: BorderRadius.circular(8),
               ),
-              padding: EdgeInsets.all(16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton.icon(
-                        onPressed: () {
-                          // Edit profile action
-                        },
-                        icon: Icon(Icons.edit), // Icon for edit action
-                        label: Text('Edit'),
-                      ),
-                    ],
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => InfoPage(),
+                              ),
+                            );
+                          },
+                          icon: Icon(Icons.info), // Information icon
+                          tooltip: 'Information',
+                        ),
+                        TextButton.icon(
+                          onPressed: () {
+                            // Edit profile action
+                          },
+                          icon: Icon(Icons.edit), // Icon for edit action
+                          label: Text('Edit'),
+                        ),
+                      ],
+                    ),
                   ),
-                  Text('Full Name: Abdulrahman Alkhalaf'),
-                  Text('Email: Abdul123@gmail.com.com'),
-                  Text('Phone Number: 0565433210'),
-                  Text('City: Jubail'),
+                  Padding(
+                    padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Full Name: Abdulrahman Alkhalaf'),
+                        Text('Email: Abdul123@gmail.com.com'),
+                        Text('Phone Number: 0565433210'),
+                        Text('City:Jubail'),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
