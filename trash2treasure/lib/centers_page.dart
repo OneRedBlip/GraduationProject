83787bb313f1617f6dd836388ad577baf14d890c
OneRedBlip <149704244+OneRedBlip@users.noreply.github.com>
2023-12-05 06:19:11 +0300

    Remove _buildRecycleCenterBox widget

--- trash2treasure/lib/centers_page.dart
+++ trash2treasure/lib/centers_page.dart
@@ -10,7 +10,6 @@ class CentersPage extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               'Recycle Centers in Saudi',
@@ -45,34 +44,7 @@ class CentersPage extends StatelessWidget {
     );
   }
 
-  Widget _buildRecycleCenterBox({required String cityName, required String imagePath}) {
-    return Container(
-      width: 120,
-      height: 120,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Stack(
-        children: [
-          Image.asset(
-            imagePath,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              cityName,
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+
 }
 
 class CenterCard extends StatelessWidget {
@@ -95,8 +67,8 @@ class CenterCard extends StatelessWidget {
     return ListTile(
       leading: Text(material),
       title: Text(name),
-      subtitle: Text('${points}pts'),
-      trailing: Text('${distance}Km'),
+      subtitle: Text('$points pts'),
+      trailing: Text('$distance Km'),
       onTap: onTap,
     );
   }
