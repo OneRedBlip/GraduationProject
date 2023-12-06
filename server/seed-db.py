8ec44668f3c681ea2e8ad87d1a53f2f39bf28f02
OneRedBlip <149704244+OneRedBlip@users.noreply.github.com>
2023-12-06 06:21:35 +0300

    Server: add random posts
    The post dates are in the future for some of them but who cares

--- server/seed-db.py
+++ server/seed-db.py
@@ -47,6 +47,9 @@ for _ in range(99):
     demo_users.append({'user_name': generate_arab_username(), 'email': generate_arab_email(
     ), 'phone_num': generate_random_phone_number(), 'user_location': generate_saudi_city()})
 
+demo_users.append({'user_name': "demouser", 'email': "DemoEmailAddress@example.com",
+                   'phone_num': generate_random_phone_number(), 'user_location': "jubail"})
+
 # Convert the list to a tuple
 users_tuple = tuple(demo_users)
 
@@ -63,4 +66,49 @@ def addDemoUsers(users: tuple[dict, ...], db) -> None:
 db = database.Database("database.db")
 db.cur.execute("UPDATE users SET points = 1750 WHERE points = 0")
 db.con.commit()
+
+demo_posts = (
+    {"user_id": 15, "post_date": 170198992, "material_type": "paper", "location": "riyadh",
+        "additional_info": "Old newspapers for recycling.", "status": "o"},
+    {"user_id": 16, "post_date": 170198992, "material_type": "glass", "location": "riyadh",
+        "additional_info": "Broken glass pieces, suitable for recycling.", "status": "o"},
+    {"user_id": 17, "post_date": 1770498992, "material_type": "plastic", "location": "riyadh",
+        "additional_info": "Empty plastic containers available for pickup.", "status": "o"},
+    {"user_id": 18, "post_date": 170198992, "material_type": "metal", "location": "riyadh",
+        "additional_info": "Scrap metal from renovation project.", "status": "o"},
+    {"user_id": 19, "post_date": 170198992, "material_type": "paper", "location": "dammam",
+        "additional_info": "Cardboard boxes for recycling.", "status": "o"},
+    {"user_id": 20, "post_date": 170198992, "material_type": "glass", "location": "dammam",
+        "additional_info": "Used glass bottles available for collection.", "status": "o"},
+    {"user_id": 21, "post_date": 170198992, "material_type": "metal", "location": "dammam",
+        "additional_info": "Copper wires and pipes from home renovation.", "status": "o"},
+    {"user_id": 22, "post_date": 2290499064, "material_type": "plastic", "location": "riyadh",
+        "additional_info": "Plastic containers and packaging materials available.", "status": "o"},
+    {"user_id": 23, "post_date": 1170498992, "material_type": "paper", "location": "jubail",
+        "additional_info": "Assorted paper materials for recycling.", "status": "o"},
+    {"user_id": 24, "post_date": 170199153, "material_type": "glass", "location": "jubail",
+        "additional_info": "Empty glass jars and bottles for pickup.", "status": "o"},
+    {"user_id": 25, "post_date": 170199076, "material_type": "metal", "location": "dammam",
+        "additional_info": "Scrap metal pieces, suitable for recycling.", "status": "o"},
+    {"user_id": 26, "post_date": 170198992, "material_type": "plastic", "location": "riyadh",
+        "additional_info": "Plastic bags and packaging available for reuse.", "status": "o"},
+    {"user_id": 27, "post_date": 170198992, "material_type": "paper", "location": "dammam",
+        "additional_info": "Unused cardboard sheets and boxes.", "status": "o"},
+    {"user_id": 28, "post_date": 170199032, "material_type": "glass", "location": "jubail",
+        "additional_info": "Assorted glass items for recycling.", "status": "o"},
+    {"user_id": 29, "post_date": 170199004, "material_type": "metal", "location": "riyadh",
+        "additional_info": "Old aluminum cans and scrap.", "status": "o"},
+    {"user_id": 30, "post_date": 170199184, "material_type": "plastic", "location": "dammam",
+        "additional_info": "Plastic bottles and containers available.", "status": "o"},
+    {"user_id": 50, "post_date": 170199022, "material_type": "plastic", "location": "dammam",
+        "additional_info": "Plastic packaging materials for recycling.", "status": "o"},
+)
+
+
+def addDemoPosts(posts: tuple[dict, ...], db: database.Database) -> None:
+    for post in posts:
+        db.insert("posts", post)
+
+
 addDemoUsers(users_tuple, db)
+addDemoPosts(demo_posts, db)
