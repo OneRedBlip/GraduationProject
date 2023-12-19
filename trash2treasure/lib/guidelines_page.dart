73840a1d591499507c28dd91cd8a058b868c2b87
OneRedBlip <149704244+OneRedBlip@users.noreply.github.com>
2023-12-19 20:19:24 +0300

    Remove text in guidelines page
    Actually, just comment it out in case we decide to keep it.

--- trash2treasure/lib/guidelines_page.dart
+++ trash2treasure/lib/guidelines_page.dart
@@ -28,11 +28,11 @@ class GuidelinesPage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: const Text(
-                  'Recyclability varies across different materials, playing a crucial role in sustainable waste management. Plastics, including PET and HDPE, are commonly recyclable, and proper disposal channels facilitate their reuse. Aluminum, known for its high recycling efficiency, is a sustainable choice due to its infinite recyclability without compromising quality. Glass is another recyclable material, with the potential to be reprocessed repeatedly. However, challenges arise with certain materials like food waste, which poses contamination issues in recycling streams. Batteries, comprising hazardous materials, should not be included in regular recycling bins, as their disposal requires specialized processes to prevent environmental harm. Understanding the recyclability of materials empowers individuals to make environmentally conscious choices in their daily lives.'),
-            ),
+            // Padding(
+            //   padding: const EdgeInsets.all(10.0),
+            //   child: const Text(
+            //       'Recyclability varies across different materials, playing a crucial role in sustainable waste management. Plastics, including PET and HDPE, are commonly recyclable, and proper disposal channels facilitate their reuse. Aluminum, known for its high recycling efficiency, is a sustainable choice due to its infinite recyclability without compromising quality. Glass is another recyclable material, with the potential to be reprocessed repeatedly. However, challenges arise with certain materials like food waste, which poses contamination issues in recycling streams. Batteries, comprising hazardous materials, should not be included in regular recycling bins, as their disposal requires specialized processes to prevent environmental harm. Understanding the recyclability of materials empowers individuals to make environmentally conscious choices in their daily lives.'),
+            // ),
             const SizedBox(height: 8),
             TextButton(
                 child: const Text('Click here for more information'),
